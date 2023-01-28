8b4e623baa951aa0db448813ddacd1f9632e24be
Sean Wong <seanwong24@live.com>
2023-01-28 15:54:50 +0000

    use index.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/hey-monaco-editor.ts',
+      entry: 'src/index.ts',
+      fileName: 'index',
       formats: ['es'],
     },
     rollupOptions: {
       // external: /^lit/,
     },
-    assetsDir: 'src/assets/'
+    assetsDir: 'src/assets/',
   },
-})
+});
