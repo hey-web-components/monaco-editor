d8d7dbb28c3afb60c5418a4f3af49740eb6975af
Sean Wong <seanwong24@live.com>
2023-01-29 11:10:14 +0000

    update types

---
+++ types/.storybook/stories/hey-monaco-diff-editor.stories.d.ts
@@ -0,0 +1,5 @@
+import { Meta, StoryObj } from '@storybook/web-components';
+import '../../src/hey-monaco-diff-editor';
+declare const _default: Meta<import("@storybook/web-components").Args>;
+export default _default;
+export declare const Default: StoryObj;
---
+++ types/src/editor-base.d.ts
@@ -0,0 +1,40 @@
+import { LitElement } from 'lit';
+import { Monaco } from '@monaco-editor/loader';
+import { editor } from 'monaco-editor';
+export declare abstract class EditorBase<T extends editor.IStandaloneCodeEditor | editor.IStandaloneDiffEditor> extends LitElement {
+    static styles: import("lit").CSSResult;
+    /**
+     * @internal
+     */
+    protected abstract readonly PROPERTY_CHANGE_HANDLER_DICT: {
+        [propertyName: string]: (value: any) => void;
+    };
+    /**
+     * @internal
+     */
+    protected editorContainerRef: import("lit-html/directives/ref").Ref<HTMLDivElement>;
+    /**
+     * After component loaded, the `Monaco` instance can be obtained using this property.
+     */
+    monaco?: Monaco;
+    /**
+     * After component loaded, the editor instance can be obtained using this property.
+     */
+    editor?: T;
+    /**
+     * The `vs` path of the monaco editor. Default to the CDN url.
+     */
+    vsPath: string;
+    /**
+     * The `options` for the editor.
+     */
+    abstract options?: editor.IEditorOptions;
+    firstUpdated(): void;
+    shouldUpdate(changedProperties: Map<string, any>): boolean;
+    render(): import("lit-html").TemplateResult<1>;
+    protected initialize(): Promise<void>;
+    protected loadMonaco(): Promise<void>;
+    protected obtainEditorCSSString(): Promise<string>;
+    protected abstract loadEditor(): Promise<void>;
+    protected abstract defineEvents(): void;
+}
