dc2b839406655317cd05325a38de38cfc86cdecc
Sean <shw940@usask.ca>
2023-06-13 11:35:47 +0000

    upgrade to storybook v7

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,13 +1,18 @@
 module.exports = {
   stories: ['./**/*.stories.mdx', './**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  framework: '@storybook/web-components',
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-mdx-gfm'],
+  framework: {
+    name: '@storybook/web-components-vite',
+    options: {}
+  },
   core: {
-    builder: '@storybook/builder-vite',
-    disableTelemetry: true,
+    disableTelemetry: true
   },
   viteFinal(config) {
     config.base = './';
     return config;
   },
+  docs: {
+    autodocs: true
+  }
 };
