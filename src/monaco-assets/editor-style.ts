2faa8c524e1c942e5e4fd87fc3a1c37fd08115c0
Sean <shw940@usask.ca>
2023-06-16 20:10:15 +0000

    fix icon loading

--- src/monaco-assets/editor-style.ts
+++ src/monaco-assets/editor-style.ts
@@ -1,3 +1,8 @@
 import _ from 'monaco-editor/min/vs/editor/editor.main.css?inline';
-const editorStyle = _ as string;
+import codicon from 'monaco-editor/min/vs/base/browser/ui/codicons/codicon/codicon.ttf?url';
+
+const editorStyle = _.replace(
+  /url(..\/base\/browser\/ui\/codicons\/codicon\/codicon.ttf)/g,
+  `url(${codicon})`,
+) as string;
 export default editorStyle;
