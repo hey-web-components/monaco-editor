e510186b6812556f2c8b6b5b9fecf93d369c1eb6
Sean Wong <seanwong24@live.com>
2024-05-14 05:47:27 +0000

    Revert "change a way to import workers"
    This reverts commit dcec9bbd97d96d1b66af77577b72762a6a671f5d.

--- src/editor-base.ts
+++ src/editor-base.ts
@@ -2,6 +2,11 @@ import {LitElement, css, html, unsafeCSS} from 'lit';
 import {property} from 'lit/decorators.js';
 import {createRef, ref} from 'lit/directives/ref.js';
 import * as monaco from 'monaco-editor';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 import monacoCSS from 'monaco-editor/min/vs/editor/editor.main.css?inline';
 
 type EditorInstance =
@@ -96,48 +101,20 @@ export abstract class EditorBase<T extends EditorInstance> extends LitElement {
 
   protected async loadMonaco() {
     self.MonacoEnvironment = {
-      getWorker: function (_workerId, label) {
-        const getWorkerModule = (moduleUrl: string, label: string) => {
-          return new Worker(
-            self.MonacoEnvironment?.getWorkerUrl?.(moduleUrl, label) ?? '',
-            {
-              name: label,
-              type: 'module',
-            }
-          );
-        };
-        switch (label) {
-          case 'json':
-            return getWorkerModule(
-              '/monaco-editor/esm/vs/language/json/json.worker?worker',
-              label
-            );
-          case 'css':
-          case 'scss':
-          case 'less':
-            return getWorkerModule(
-              '/monaco-editor/esm/vs/language/css/css.worker?worker',
-              label
-            );
-          case 'html':
-          case 'handlebars':
-          case 'razor':
-            return getWorkerModule(
-              '/monaco-editor/esm/vs/language/html/html.worker?worker',
-              label
-            );
-          case 'typescript':
-          case 'javascript':
-            return getWorkerModule(
-              '/monaco-editor/esm/vs/language/typescript/ts.worker?worker',
-              label
-            );
-          default:
-            return getWorkerModule(
-              '/monaco-editor/esm/vs/editor/editor.worker?worker',
-              label
-            );
+      getWorker(_, label) {
+        if (label === 'json') {
+          return new jsonWorker();
         }
+        if (label === 'css' || label === 'scss' || label === 'less') {
+          return new cssWorker();
+        }
+        if (label === 'html' || label === 'handlebars' || label === 'razor') {
+          return new htmlWorker();
+        }
+        if (label === 'typescript' || label === 'javascript') {
+          return new tsWorker();
+        }
+        return new editorWorker();
       },
     };
   }
